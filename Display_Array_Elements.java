5d8d28d61df9b4a04c8767b2bd15709da2e4e1d0
OMKESALKAR <omkesalkar0@gmail.com>
2024-12-20 14:51:32 +0530

    Add files via upload

---
+++ Display_Array_Elements.java
@@ -0,0 +1,15 @@
+package Logical_Que.Dec_19;
+
+public class Display_Array_Elements 
+{
+	public static void main(String[] args) 
+	{
+		int[] arr = {10, 20, 30, 40, 50};
+		
+		for(int i=0; i < arr.length; i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+
+}
